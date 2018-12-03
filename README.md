f3e2f1b798853dbca2d6bf79b9888601bbc58340
bdriesen <bram.driesen@capgemini.com>
2018-12-03 12:13:46 +0100

    Added the basic result's table.

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 End Of Life Car Lease Calculator
 
 Simple static html site with Javascript logic to calculate the end of your car lease.
+
+## Usage
+
+The page can be viewed and used on GitHub Pages at the following URL: https://bramdriesen.github.io/eolcarleasecalc/
+
+## Awesome tools and libraries used in this project:
+- Javascript Cookies
+- Cookieconsent
+- Moment.js
+
+## Contributing
+If you feel like something isn't quite right, or if you have amazing improvements to offer! Feel free to create a pull request or submit an issue into the issue queue.
--- assets/js/complete_calculation.js
+++ assets/js/complete_calculation.js
@@ -13,21 +13,61 @@ $('#calculate').on('click', function () {
     let weeklyResult = [];
     let totalWeeklyMileage = 0;
 
-    $.each(weeklyMileages, function(key, value) {
+    $.each(weeklyMileages, function (key, value) {
         weeklyResult[key] = value.value;
     });
 
-    $.each(weeklyTimes, function(key, value) {
+    $.each(weeklyTimes, function (key, value) {
         let mileage = weeklyResult[key];
-        // TODO: The re-adding to the array isn't really needed.
         weeklyResult[key] = mileage * value.value;
         totalWeeklyMileage += mileage * value.value;
     });
 
     let tableDiv = $('#result');
-    tableDiv.append('<h2>Result</h2>')
+    // Empty the current result div and re-append the header and table opening.
+    tableDiv.empty();
+    tableDiv.append('<h2>Result</h2>');
+    let tableHtml = '<table class="table">';
+    // TABLE - HEAD.
+    tableHtml += '<thead> <tr> <th scope="col">Year</th> <th scope="col">Month</th> <th scope="col">Mileage (est)</th> <th scope="col">#</th> </tr> </thead> <tbody>';
 
-    let foo = '<table class="table"> <thead> <tr> <th scope="col">#</th> <th scope="col">First</th> <th scope="col">Last</th> <th scope="col">Handle</th> </tr></thead> </table>';
-    tableDiv.append(foo);
 
+    // Cast the contract start into a start date.
+    let startDate = moment(contractStart);
+    let endDate = moment(contractStart);
+    // Calculate the end date.
+    endDate.add(contractDuration, 'months');
+
+    // Create an array of all the years that we need to fill with data.
+    let years = [];
+    for (let i = parseInt(startDate.format('YYYY')); i <= parseInt(endDate.format('YYYY')); i++) {
+        years.push(i);
+    }
+
+
+    years.forEach(function (year) {
+        moment.months().forEach(function (month, index) {
+            tableHtml += '<tr>';
+            // If the index is 0 we add the year.
+            if (index === 0) {
+                tableHtml += '<td rowspan="12">' + year + '</td>';
+            }
+
+            tableHtml += '<td>' + month + '</td>';
+
+            // TODO: Mileage.
+            tableHtml += '<td>123</td>';
+
+            // TODO: If over mileage add td.
+            // TODO: If month == end of lease, add td.
+            tableHtml += '<td style="color: tomato">End of Lease!</td>';
+
+            tableHtml += '</tr>';
+        });
+    });
+
+
+    // Close the table and append.
+    tableHtml += '</tbody></table>';
+    tableDiv.append(tableHtml);
 });
