152dae28e831246b7b0fb9578fb212070eb6c5a9
bdriesen <bram.driesen@capgemini.com>
2018-12-04 08:59:51 +0100

    Added favicon and fixed a bug in the cookies, cookies don't work for weekly mileage yet.

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -6,6 +6,9 @@ $(document).ready(function () {
     $('input[name=contractualLimit]').val( Cookies.get('contractualLimit') );
     $('input[name=contractStart]').val( Cookies.get('contractStart') );
     $('input[name=contractDuration]').val( Cookies.get('contractDuration') );
+    // TODO: This is not working.
+    // TODO: We need to add html elements for each item.
+    // TODO: We also need to store the note.
     $('input[name="weeklyMileage[]"]').val( Cookies.get('weeklyMileages') );
     $('input[name="weeklyTimes[]"]').val( Cookies.get('weeklyTimes') );
 });
@@ -14,7 +17,7 @@ $(document).ready(function () {
 // * COOKIE SET * //.
 // ************** //.
 $('#calculate').on('click', function () {
-    Cookies.set('current_mileage', $('input[name=currentMileage]').val());
+    Cookies.set('currentMileage', $('input[name=currentMileage]').val());
     Cookies.set('contractualLimit', $('input[name=contractualLimit]').val());
     Cookies.set('contractStart', $('input[name=contractStart]').val());
     Cookies.set('contractDuration', $('input[name=contractDuration]').val());
